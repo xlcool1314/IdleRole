7267db7f5f2566d69a6338d0e911044073419ab7
XL <xlcool05@qq.com>
2020-01-18 23:48:25 +0800

    添加脚本

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonMono<GameManager>
+{
+    public void Gust(){
+        Debug.Log("test");
+        Hid();
+    }
+
+    void Hid(){
+        Debug.Log("hello");
+    }
+
+}
