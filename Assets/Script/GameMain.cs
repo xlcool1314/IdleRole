0c3579245ceab1361b8649b5732c0b3ae6fb1b31
xianglei <xianglei@thinkplay.co>
2020-01-19 18:55:58 +0800

    添加GameMain

--- Assets/Script/GameMain.cs
+++ Assets/Script/GameMain.cs
@@ -7,7 +7,7 @@ public class GameMain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameManager.Instance.Gust();
+        
     }
 
     // Update is called once per frame
