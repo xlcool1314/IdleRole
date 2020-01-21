a4d3c833d79f14adc9dd66dc8bfcf969835dc5bf
xianglei <xianglei@thinkplay.co>
2020-01-21 14:49:02 +0800

    资源管理上传

--- Assets/Script/Framework/SingletonMono.cs
+++ Assets/Script/Framework/SingletonMono.cs
@@ -1,21 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
-public class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
+public class GameMangerBehavaiour : MonoBehaviour
 {
-private static T instance;
-public static T Instance {
-    get {
-        if(instance == null)
+
+    /// <summary>
+    /// 初始化游戏
+    /// </summary>
+    /// <returns></returns>
+    public virtual Task Init() { return null; }
+
+    /// <summary>
+    /// 开始一局
+    /// </summary>
+    /// <returns></returns>
+    public virtual Task StartGame() { return null; }
+
+    /// <summary>
+    /// 游戏结束, 这里并没有清理内容，还在等待玩家确认游戏结果
+    /// </summary>
+    /// <returns></returns>
+    public virtual Task EndGame() { return null; }
+
+    /// <summary>
+    /// 清理游戏资源
+    /// </summary>
+    /// <returns></returns>
+    public virtual Task GameClean() { return null; }
+
+    /// <summary>
+    /// 对局逻辑循环
+    /// </summary>
+    /// <returns></returns>
+    public virtual Task UpdateGameLoop(float deltaTime) { return null; }
+
+    /// <summary>
+    /// 全局Update
+    /// </summary>
+    /// <returns></returns>
+    public virtual Task UpdateLoop() { return null; }
+
+    /// <summary>
+    /// 全局LateUpdate
+    /// </summary>
+    /// <returns></returns>
+    public virtual Task LateUpdateLoop() { return null; }
+}
+
+
+public class SingletonMono<T> : GameMangerBehavaiour where T : GameMangerBehavaiour
+{
+    private static T instance = null;
+
+    public static T Instance
+    {
+        get
         {
-            GameObject obj = new GameObject();
-            instance = obj.AddComponent<T>();
-            obj.name = typeof(T).Name;
-            //切换场景之后不销毁单例对象
-            DontDestroyOnLoad(obj);
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<T>();
+
+                if (instance == null)
+                {
+                    //Debug.Log($"Instancing MonoBehaviour: <color=#00ff00>{typeof(T)}</color>");
+                    GameObject go = new GameObject("Temp");
+                    instance = go.AddComponent<T>();
+                    go.name = instance.GetType().FullName;
+                }
+            }
+
+            return instance;
         }
-        return instance;
     }
 }
-}
+
--- Assets/Script/Manager/UiManager.cs
+++ Assets/Script/Manager/UiManager.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class UiManager : SingletonMono<UiManager>
 {
-    
+    public override Task Init()
+    {
+        return base.Init();
+    }
 }
