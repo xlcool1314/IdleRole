b149d77f04cd31b400651aef62e9fe4b23de1d37
xianglei <xianglei@thinkplay.co>
2020-01-21 13:13:32 +0800

    上传txtpro

---
+++ Assets/Script/GameData/LevelData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+[CreateAssetMenu(menuName = "Config/Create LevelData ")]
+public class LevelData : ScriptableObject
+{
+    public List<LevelDesc> levelinfo;
+}
+
+[System.Serializable]
+public class LevelDesc
+{
+    public int level;
+}
+
