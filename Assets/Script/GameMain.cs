b7a9dcb6e8d2abe59bd0db08fd616cd8d94eb5c4
xianglei <xianglei@thinkplay.co>
2020-01-21 15:52:57 +0800

    资源管理上传

--- Assets/Script/GameMain.cs
+++ Assets/Script/GameMain.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class GameMain : SingletonMono<GameMain>
 {
-    
+
+    public override Task Init()
+    {
+        return base.Init();
+    }
 }
---
+++ Assets/Script/Manager/UIBaseBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public abstract class UIBaseBehaviour : MonoBehaviour
+{
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public abstract Task Init();
+    public abstract void UpdateInfo(float deltaTime);
+    public abstract void Clean();
+}
