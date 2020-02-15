363671866772cde91686c7d0c9212c1ce65519cd
XL <xlcool05@qq.com>
2020-02-15 22:49:57 +0800

    死亡判断条件

--- Assets/Script/Role/BattlefieldMonitor.cs
+++ Assets/Script/Role/BattlefieldMonitor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BattlefieldMonitor : SingletonMono<BattlefieldMonitor>
 {
@@ -13,6 +14,8 @@ public class BattlefieldMonitor : SingletonMono<BattlefieldMonitor>
     {
         FindAllMyRole();
         FindAllEnemys();
+        GameOver();
+
     }
 
     public void FindAllMyRole()//找到所有的我方角色
@@ -24,5 +27,13 @@ public class BattlefieldMonitor : SingletonMono<BattlefieldMonitor>
     {
         allEnemys=GameObject.FindGameObjectsWithTag("EnemysPlane");
 
+    }
+
+    void GameOver()
+    {
+        if (allMyRoles.Length == 0)
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
