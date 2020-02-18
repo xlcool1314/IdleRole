e1ea3386402a69c7e790147550e30f1afb88d141
xianglei <xlcool05@qq.com>
2020-02-18 17:09:30 +0800

    增加存储

---
+++ Assets/Script/Manager/GameSaveManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class GameSaveManager : SingletonMono<GameSaveManager>
+{
+    public AllRoleData myAllRoles;
+
+    public void SaveGame()
+    {
+        Debug.Log(Application.persistentDataPath);
+        if (!Directory.Exists(Application.persistentDataPath+"/SaveData"))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/SaveData");
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();//二进制转化
+
+        FileStream file = File.Create(Application.persistentDataPath + "/SaveData/myAllRoles.txt");
+
+        var json = JsonUtility.ToJson(myAllRoles);
+
+        bf.Serialize(file, json);
+
+        file.Close();
+    }
+
+    public void LoadGame()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+
+        if(File.Exists(Application.persistentDataPath + "/SaveData/myAllRoles.txt"))
+        {
+            FileStream file = File.Open(Application.persistentDataPath + "/SaveData/myAllRoles.txt", FileMode.Open);
+
+            JsonUtility.FromJsonOverwrite((string)bf.Deserialize(file), myAllRoles);
+
+            file.Close();
+        }
+    }
+}
--- Assets/Script/Manager/UserAssetManager.cs
+++ Assets/Script/Manager/UserAssetManager.cs
@@ -14,43 +14,11 @@ public class UserAsset
     public int HaveRoles { get => haveRoles; set => haveRoles = value; }
     public int Gold { get => gold; set => gold = value; }
     public int Level { get => level; set => level = value; }
-
-    public void Save()
-    {
-       var json = JsonUtility.ToJson(this);
-       Debug.Log("Save " + json);
-       PlayerPrefs.SetString("UserAsset", json);
-       PlayerPrefs.Save();
-    }
 }
 
 
 public class UserAssetManager:SingletonMono<UserAssetManager>{
 
     UserAsset asset = new UserAsset();
-
-    bool inited = false;
-
-    public override async Task Init()
-    {
-        if (inited)
-        {
-            return;
-        }
-
-        var json = PlayerPrefs.GetString("UserAsset", "{}");
-        //Debug.Log("Load " + json);
-        asset = JsonUtility.FromJson<UserAsset>(json);
-        inited = true;
-    }
-
-    public int AddLevel(int thisLevel)
-    {
-        if (thisLevel == 0)
-        {
-            return 0;
-        }
-        return asset.Level = thisLevel + 1;
-    }
 
 }
