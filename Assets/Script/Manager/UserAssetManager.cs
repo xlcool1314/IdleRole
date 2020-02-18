dd57495a14a01bd411adbd06de07d0bfe6702a2e
xianglei <xlcool05@qq.com>
2020-02-18 17:48:28 +0800

    调整资产

--- Assets/Script/Manager/UserAssetManager.cs
+++ Assets/Script/Manager/UserAssetManager.cs
@@ -7,13 +7,8 @@ public class UserAsset
 {
     private int gold;
 
-    private int haveRoles;
-
     private int level;
 
-    public int HaveRoles { get => haveRoles; set => haveRoles = value; }
-    public int Gold { get => gold; set => gold = value; }
-    public int Level { get => level; set => level = value; }
 }
 
 
