5ff9c815ee5eeb0b12f0694d72a1be43fd6768de
xianglei <xlcool05@qq.com>
2020-02-21 16:31:01 +0800

    调整数据结构

--- Assets/Script/Config/AllRoleData.cs
+++ Assets/Script/Config/AllRoleData.cs
@@ -12,6 +12,7 @@ public class AllRoleData : ScriptableObject
 [System.Serializable]
 public class RoleData
 {
+    public string name;
     public List<RoleInfo> RoleInfo;//某个角色信息
 }
 
@@ -22,39 +23,6 @@ public class RoleInfo
 
     public string describe;//描述
 
-    public int myhp;//血量
+    public GameObject roleObj;//怪物预制体
 
-    public int maxHp;//最大血量
-
-    public int defense;//防御
-
-    public int mindamage;//最小攻击力
-
-    public int maxdamage;//最大攻击力
-
-    public int myTreatment;// 治疗量
-
-    public float maxAttackSpeed;//最大的的攻击间隔
-
-    public Animator myAnimator;//动画控制器
-
-    public GameObject attackEffects;//攻击特效
-
-    public GameObject underAttackEffects;//受击特效
-
-    public GameObject deadEffects;//死亡特效
-
-    public BattlefieldMonitor allRoles;//所有角色
-
-    public Stat hpBar;//血条
-
-    public Stat attackSpeedBar;//攻击频率条
-
-    public LossHp lossHpText;//显示伤害数字的脚本
-
-    public LossHp treatmentText;//显示的治疗量
-
-    public Animator lossAnimator;//伤害数字的显示动画
-
-    public Animator treatmentAnimator;//治疗数字显示动画
 }
