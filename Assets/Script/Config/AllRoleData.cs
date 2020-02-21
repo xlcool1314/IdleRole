f1c7804fa37c8a91baa82734f96c46a672072b85
xianglei <xlcool05@qq.com>
2020-02-21 14:31:47 +0800

    所有角色数据

--- Assets/Script/Config/AllRoleData.cs
+++ Assets/Script/Config/AllRoleData.cs
@@ -2,17 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AllRoleData : MonoBehaviour
+[CreateAssetMenu(menuName = "Config/Create AllRoleData")]
+public class AllRoleData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<RoleData> AllRoleInfo;//所有角色信息
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+}
+
+[System.Serializable]
+public class RoleData
+{
+    public List<RoleInfo> RoleInfo;//某个角色信息
+}
+
+[System.Serializable]
+public class RoleInfo
+{
+    public string name;//怪物名称
+
+    public string describe;//描述
+
+    public int myhp;//血量
+
+    public int maxHp;//最大血量
+
+    public int defense;//防御
+
+    public int mindamage;//最小攻击力
+
+    public int maxdamage;//最大攻击力
+
+    public int myTreatment;// 治疗量
+
+    public float maxAttackSpeed;//最大的的攻击间隔
+
+    public Animator myAnimator;//动画控制器
+
+    public GameObject attackEffects;//攻击特效
+
+    public GameObject underAttackEffects;//受击特效
+
+    public GameObject deadEffects;//死亡特效
+
+    public BattlefieldMonitor allRoles;//所有角色
+
+    public Stat hpBar;//血条
+
+    public Stat attackSpeedBar;//攻击频率条
+
+    public LossHp lossHpText;//显示伤害数字的脚本
+
+    public LossHp treatmentText;//显示的治疗量
+
+    public Animator lossAnimator;//伤害数字的显示动画
+
+    public Animator treatmentAnimator;//治疗数字显示动画
 }
