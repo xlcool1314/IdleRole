18090e75ee1623fa6f6be4024be99f91907d4a76
xl <xlcool05@qq.com>
2020-03-01 14:32:56 +0800

    添加游戏结束UI

---
+++ Assets/Script/Ui/GameOverUi.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+
+public class GameOverUi : UIBaseBehaviour
+{
+    public Button button;
+
+    public override void Clean()
+    {
+        button.onClick.RemoveAllListeners();
+    }
+
+    public override void Init()
+    {
+        button.onClick.AddListener(BackHome);
+    }
+
+    public override void UpdateInfo(float deltaTime)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    void BackHome()
+    {
+        SceneManager.LoadScene(0);
+        Clean();
+        Destroy(gameObject);
+    }
+}
