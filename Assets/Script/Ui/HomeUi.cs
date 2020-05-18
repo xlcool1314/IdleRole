bc156365f348ef505d568dd127646f439e14d22b
xianglei <xlcool05@qq.com>
2020-05-18 15:36:01 +0800

    增加计时器

--- Assets/Script/Ui/HomeUi.cs
+++ Assets/Script/Ui/HomeUi.cs
@@ -19,6 +19,32 @@ public class HomeUi : UIBaseBehaviour
 
     public TextMeshProUGUI gold;
 
+
+    public TextMeshProUGUI m_ClockText;
+    private float m_Timer;
+    private int m_Hour;//时
+    private int m_Minute;//分
+    private int m_Second;//秒
+    public void Clock()
+    {
+        m_Timer += Time.deltaTime;
+        m_Second = (int)m_Timer;
+        if (m_Second > 59.0f)
+        {
+            m_Second = (int)(m_Timer - (m_Minute * 60));
+        }
+        m_Minute = (int)(m_Timer / 60);
+        if (m_Minute > 59.0f)
+        {
+            m_Minute = (int)(m_Minute - (m_Hour * 60));
+        }
+        m_Hour = m_Minute / 60;
+        if (m_Hour >= 24.0f)
+        {
+            m_Timer = 0;
+        }
+        m_ClockText.text = string.Format("{0:d2}:{1:d2}:{2:d2}", m_Hour, m_Minute, m_Second);
+    }
     public override void Clean()
     {
 
@@ -44,7 +70,7 @@ public class HomeUi : UIBaseBehaviour
 
     private void Update()
     {
-
+        Clock();
     }
 
     public void CreatShopUi()
