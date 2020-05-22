69d0f728d254bccc70be0930532fa76c5aa4f5da
xianglei <xlcool05@qq.com>
2020-05-22 17:48:00 +0800

    增加资源的开启加载

--- Assets/Script/Manager/ResourcesManager.cs
+++ Assets/Script/Manager/ResourcesManager.cs
@@ -1,9 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
 public class ResourcesManager : SingletonMono<ResourcesManager>
 {
-    
+    public GameObject shopRoleCell;
+
+    public GameObject combinationUi;
+
+    public GameObject buyRoleUi;
+
+    public GameObject logoUi;
+
+    public GameObject gameOverUi;
+
+    public GameObject shopUi;
+
+
+
+
+    public async Task InitResources()
+    {
+         var task001 = Addressables.LoadAssetAsync<GameObject>("ShopRoleCell").Task;
+        shopRoleCell = await task001;
+
+        var task002 = Addressables.LoadAssetAsync<GameObject>("ShopRoleCell").Task;
+        combinationUi = await task002;
+
+        var task003 = Addressables.LoadAssetAsync<GameObject>("ShopRoleCell").Task;
+        buyRoleUi = await task003;
+
+        var task004 = Addressables.LoadAssetAsync<GameObject>("ShopRoleCell").Task;
+        logoUi = await task004;
+
+        var task005 = Addressables.LoadAssetAsync<GameObject>("ShopRoleCell").Task;
+        gameOverUi = await task005;
+
+        var task006 = Addressables.LoadAssetAsync<GameObject>("ShopRoleCell").Task;
+        shopUi = await task006;
+
+
+
+    }
+
+
+
+
 }
