97fc7f3950b7bb64deab5a90cf9f5de42716b321
xianglei <xlcool05@qq.com>
2020-05-23 22:00:54 +0800

    角色信息弹窗

--- Assets/Script/Role/RoleWindowInfo.cs
+++ Assets/Script/Role/RoleWindowInfo.cs
@@ -14,7 +14,7 @@ public class RoleWindowInfo : MonoBehaviour
 
     public void LvUp()
     {
-        Instantiate(ResourcesManager.Instance.roleInfo, transform.position,Quaternion.identity,gameObject.transform.parent);
+        Instantiate(ResourcesManager.Instance.roleInfo, transform.position,Quaternion.identity,GameObject.Find("GameMain").transform);
     }
 
     // Update is called once per frame
