da64dc7b89a7c65e85614ae819c4be6d9397b0b1
xianglei <xlcool05@qq.com>
2020-05-26 15:59:44 +0800

    添加音乐管理器

--- Assets/Script/Effect/DeleteEffect.cs
+++ Assets/Script/Effect/DeleteEffect.cs
@@ -7,6 +7,6 @@ public class DeleteEffect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject,2f);
+        Destroy(gameObject,1.5f);
     }
 }
---
+++ Assets/Script/Manager/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class SoundManager : SingletonMono<SoundManager>
+{
+    AudioSource audioSource;
+    public override Task Init()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        PlayBGM("Happy_new_year");
+        return null;
+    }
+
+    public async void Play(string name, Vector3 pos)
+    {
+        var clip = await Addressables.LoadAssetAsync<AudioClip>(name).Task;
+        AudioSource.PlayClipAtPoint(clip, pos);
+    }
+
+    public async void PlayBGM(string name)
+    {
+        audioSource.clip = await Addressables.LoadAssetAsync<AudioClip>(name).Task;
+        audioSource.Play();
+        audioSource.loop = true;
+    }
+
+    public void Mute(bool val)
+    {
+        audioSource.mute = val;
+    }
+}
